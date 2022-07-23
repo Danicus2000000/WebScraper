c8ced8eb7e1aaae5f4e1cbc2ebaf1470c4681405
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2022-07-23 22:52:11 +0100

    Add project files.

---
+++ WebScraper/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+while (true)
+{
+    string result = "";
+    #region Request
+    using (HttpClient getData = new())
+    {
+        bool pass = true;
+        while (pass)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a web address to scrape:");
+                string address = "";
+                address = Console.ReadLine();
+                result = getData.GetAsync(address).Result.Content.ReadAsStringAsync().Result;
+                pass = false;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The URL provided was invalid!");
+                pass = true;
+            }
+        }
+        result.Replace(">", ">\n");
+        Console.WriteLine(result);
+    }
+    #endregion
+    #region Save
+    char key;
+    while (true) 
+    {
+        Console.WriteLine("Would you like to save the data to a file? (Y/N)");
+        key=Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        if(key=='y' || key == 'n') 
+        {
+            break;
+        }
+    }
+    if (key == 'y') 
+    {
+        string filename = "";
+        Console.WriteLine("Please enter the filename: ");
+        filename = Console.ReadLine();
+        StreamWriter write=File.CreateText(filename+".txt");
+        write.Write(result);
+        write.Close();
+        Console.WriteLine("File Saved!");
+    }
+    #endregion
+    #region again
+    char restartKey;
+    while (true) 
+    {
+        Console.WriteLine("Would you like to scrape another page? (Y/N)");
+        restartKey=Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        if (restartKey=='y' || restartKey == 'n')
+        {
+            break;
+        }
+    }
+    if (restartKey == 'n') 
+    {
+        break;
+    }
+    #endregion
+    Console.Clear();
+}
