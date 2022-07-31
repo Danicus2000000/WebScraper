e1162fe7be1056751c125a754eb3620abbb0e6b4
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2022-07-31 17:39:30 +0100

    Added save to HTML button and allowed for enter key to be pressed in textbox to scrape

--- WebScraperApp/MainWindow.xaml.cs
+++ WebScraperApp/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
+using System.IO;
 namespace WebScraperApp
 {
     /// <summary>
@@ -27,7 +28,7 @@ namespace WebScraperApp
 
         private void url_txt_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (url_txt.Text == "Please Enter a URL")
+            if (url_txt.Text == "Please Enter a URL")//clear url text on click in
             {
                 url_txt.Clear();
                 url_txt.Opacity = 100;
@@ -36,7 +37,7 @@ namespace WebScraperApp
 
         private void scrape_btn_Click(object sender, RoutedEventArgs e)
         {
-            try
+            try//atempt scrape
             {
                 using (WebClient getData = new())
                 {
@@ -44,9 +45,41 @@ namespace WebScraperApp
                 }
                 result_txt.Visibility=Visibility.Visible;
             }
-            catch (Exception)
+            catch (Exception)//if invalid Url
             {
                 MessageBox.Show("The URL provided was invalid!","Error");
+            }
+        }
+
+        private void save_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(result_txt.Text != "") 
+            {
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.FileName = "Scrape"; // Default file name
+                dlg.DefaultExt = ".html"; // Default file extension
+                dlg.Filter = "Web Pages (.html)|*.html"; // Filter files by extension
+
+                // Show save file dialog box
+                Nullable<bool> result = dlg.ShowDialog();
+
+                // Process save file dialog box results
+                if (result == true)
+                {
+                    // Save document
+                    StreamWriter write = File.CreateText(dlg.FileName);
+                    write.Write(result_txt.Text);
+                    write.Close();
+                    MessageBox.Show(dlg.FileName.Split("\\").Last() + " has been saved!", "File Saved");
+                }
+            }
+        }
+
+        private void url_txt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter) //when enter is pressed trigger scrape
+            {
+                scrape_btn_Click(this, e);
             }
         }
     }
